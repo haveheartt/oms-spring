8662c05c94dd55d5f6d729b4199e2805df7e39b1
gui <guilhermev2huehue@gmail.com>
2024-07-22 22:08:58 -0300

    feat: creating listener on rabbitmq queue

---
+++ src/main/java/com/microservice/oms/listener/dto/OrderCreatedEvent.java
@@ -0,0 +1,8 @@
+package com.microservice.oms.listener.dto;
+
+import java.util.List;
+
+public record OrderCreatedEvent(Long codigoPedido,
+                                Long codigoCliente,
+                                List<OrderItemEvent> itens) {
+}
